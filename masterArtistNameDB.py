46d2723144a0c78c46713424d06433c8cc43962f
tgadf <tgadfort@gmail.com>
2021-10-02 17:55:35 -0500

    Saving renames

--- masterArtistNameDB.py
+++ masterArtistNameDB.py
@@ -1,198 +1,173 @@
 from ioUtils import getFile, saveFile
-from fsUtils import isFile
+from fsUtils import isDir, setDir, setFile
+from timeUtils import timestat
+from pandas import Series
 from sys import prefix
-from os import getcwd
-from os.path import join
-from searchUtils import findNearest
 
 class masterArtistNameDB:
     def __init__(self, source, debug=False, init=False):
         self.debug = debug
-        if debug:
-            print("="*25,"masterArtistNameDB","="*25)
+        print("{0} masterArtistNameDB(\"{1}\") {2}".format("="*25,source,"="*25))
         self.debug  = debug
         self.source = source
+                
+        self.musicNamesDir = setDir(prefix, 'musicnames')
+        if not isDir(self.musicNamesDir):
+            raise ValueError("There is no music names directory [{0}]".format(self.musicNamesDir))
         
-        self.artistNameDBname      = join(prefix, 'musicnames', "{0}ArtistNameDB.p".format(source))
-        self.dbRenamesname         = join(prefix, 'musicnames', "{0}DBRenames.yaml".format(source))
-        if self.debug:
-            print("  Loading data from {0}".format(self.artistNameDBname))
-        if isFile(self.artistNameDBname) and init == False:
-            self.artistNameDB = getFile(self.artistNameDBname)
+        self.manualRenames = self.getData(fast=True, local=False)
+        retval,manualRenames  = self.duplicateIndexTest()
+        if retval is False:
+            raise ValueError("There are duplicate key,values in the [{0}] data".format(self.source))
+        self.manualRenames = manualRenames
+        retval = self.recursiveTest()
+        if retval is False:
+            raise ValueError("There are recursive key,values in the [{0}] data".format(self.source))
+        self.summary()
+            
+    def summary(self, manualRenames=None):
+        manualRenames = self.manualRenames if manualRenames is None else manualRenames
+        print("masterArtistNameDB(\"{0}\") Summary:".format(self.source))
+        print("  Entries: {0}".format(manualRenames.shape[0]))
+        print("  Artists: {0}".format(manualRenames.nunique()))
+
+            
+    #########################################################################################################
+    #
+    # I/O
+    #
+    #########################################################################################################
+    def getFilename(self, fast, local):
+        basename="ManualRenames"
+            
+        self.localpfname = "{0}{1}.p".format(self.source, basename)
+        self.localyfname = "{0}{1}.yaml".format(self.source, basename)
+        self.pfname = setFile(self.musicNamesDir, self.localpfname)
+        self.yfname = setFile(self.musicNamesDir, self.localyfname)
+        
+        if fast is True:
+            if local is True:
+                return self.localpfname
+            else:
+                return self.pfname
         else:
-            if self.debug:
-                print("  Initializing a fresh DB for {0}".format(self.source))
-            self.artistNameDB = {}
-        self.checkForRecursives()
-        self.loadRenames()
-        if self.debug:
-            print("  There are currently {0} artist keys.".format(len(self.artistNameDB)))
-            print("  There are currently {0} renamed artist keys.".format(len(self.dbRenames)))
+            if local is True:
+                return self.localyfname
+            else:
+                return self.yfname
+            
+        raise ValueError("Somehow didn't get a filename!")
+
+    def getData(self, fast=True, local=False):
+        ftype = {True: "Pickle", False: "YAML"}
+        ltype = {True: "Local", False: "Main"}
+        ts = timestat("Getting Manual Renames Data From {0} {1} File".format(ltype[local], ftype[fast]))
+        fname = self.getFilename(fast, local)
+        manualRenames = getFile(fname)
+
+        ts.stop()
+    
+        return manualRenames
+
+    def writeToLocalYamlFromMainPickle(self):
+        ts = timestat("Writing To Local YAML From Main Pickle")
+        manualRenames = self.getData(fast=True, local=False)
+        self.saveData(manualRenames, fast=False, local=True)
+        ts.stop()
+
+    def writeToMainPickleFromLocalYAML(self):
+        ts = timestat("Writing To Main Pickle From Local YAML")
+        manualRenames = self.getData(fast=False, local=True)
+        self.saveData(manualRenames, fast=True, local=False)
+        ts.stop()
+        
+    def saveData(self, manualRenames=None, fast=True, local=False):
+        ftype = {True: "Pickle", False: "YAML"}
+        ltype = {True: "Local", False: "Main"}
+        ts = timestat("Saving Manual Renames Data To {0} {1} File".format(ltype[local], ftype[fast]))
+        manualRenames = self.manualRenames if manualRenames is None else manualRenames
+        self.summary(manualRenames)
+        
+        fname = self.getFilename(fast, local)
+        if fast:
+            toSave = Series(manualRenames) if isinstance(manualRenames, dict) else manualRenames
+        else:
+            toSave = manualRenames.to_dict() if isinstance(manualRenames, Series) else manualRenames
+        saveFile(idata=toSave, ifile=fname)
+        
+        ts.stop()
+        
+        
+    ##########################################################################################
+    #
+    # Assert/Consistency
+    #
+    ##########################################################################################
+    def duplicateIndexTest(self, manualRenames=None):
+        manualRenames = self.manualRenames if manualRenames is None else manualRenames
+        manualRenames = Series(manualRenames) if not isinstance(manualRenames, Series) else manualRenames
+        
+        err = False
+        for index in manualRenames.index[manualRenames.index.duplicated()]:
+            if manualRenames[index].nunique() > 1:
+                print("Found Multiple Values For Same Index [{0}]".format(index))
+                for key,value in manualRenames[index].iteritems():            
+                    print("{0: >50} <-> {1: <50}".format(key,value))
+                    err = True
+
+        _ = print("  No duplicate key/values in manual renames") if err is False else None
+        manualRenames = Series(manualRenames.to_dict()) if err is False else manualRenames
+        retval = not err
+        return retval,manualRenames
+        
+    def recursiveTest(self, manualRenames=None):
+        manualRenames = self.manualRenames if manualRenames is None else manualRenames
+        manualRenames = Series(manualRenames) if not isinstance(manualRenames, Series) else manualRenames
+        
+        retval = manualRenames[manualRenames.apply(lambda x: manualRenames.index.__contains__(x))]
+        if retval.shape[0] == 0:
+            print("  No recursive key/values in manual renames")
+            return True
+        else:
+            recursives = retval.unique()
+            print("="*105)
+            print("Found {0} recursive key/values in manual renames".format(len(recursives)))
+            for artistName in recursives:
+                print("{0: >50} <-> {1: <50}".format(artistName,manualRenames[artistName]))
+            print("="*105)
+        return False
 
         
     ##########################################################################################
+    #
     # Global Getting Function
+    #
     ##########################################################################################
     def renamed(self, artistName):
-        if self.dbRenames.get(artistName) is not None:
-            return self.dbRenames[artistName]
+        if self.manualRenames.index.__contains__(artistName):
+            return self.manualRenames[artistName]
         return artistName
-        
-        
-        
-    ##########################################################################################
-    # Saving Function
-    ##########################################################################################
-    def save(self):
-        print("Saving {0} artist keys to {1}".format(len(self.artistNameDB), self.artistNameDBname))
-        saveFile(idata=self.artistNameDB, ifile=self.artistNameDBname, debug=True)
-        
-    def loadRenames(self):
-        self.dbRenames = {}
-        for artistName,namesToBeFixed in self.artistNameDB.items():
-            for nameToBeFixed in namesToBeFixed:
-                self.dbRenames[nameToBeFixed] = artistName
-        
-    def saveRenames(self):
-        print("Saving {0} renamed artist keys to {1}".format(len(self.dbRenames), self.dbRenamesname))
-        saveFile(idata=self.dbRenames, ifile=self.dbRenamesname, debug=True)
-        
-        
-    def forceReloadNames(self, artistNameDB, save=False):
-        print("Forcing Reload Using {0} Rename Names".format(len(artistNameDB)))
-        dbRenames = {}
-        for artistName,namesToBeFixed in artistNameDB.items():
-            for nameToBeFixed in namesToBeFixed:
-                dbRenames[nameToBeFixed] = artistName
-        print("  Forced reload of {0} artist names".format(len(dbRenames)))
-        print("  Found {0} renamed names".format(len(artistNameDB)))
-        self.checkForRecursives(artistNameDB)
-        
-        self.artistNameDB = artistNameDB
-        self.dbRenames    = dbRenames
-        if save is True:
-            print("Will save everything")
-            self.save()
-            self.loadRenames()
-            self.saveRenames()
-            
-            
-    def forceReload(self, dbRenames, save=False):
-        self.forceReloadRenames(dbRenames, save)
-        
-    def forceReloadRenames(self, dbRenames, save=False):
-        print("Forcing Reload Using {0} Rename Entries".format(len(dbRenames)))
-        artistNameDB = {}
-        for olderArtistName,fixedArtistName in dbRenames.items():
-            if artistNameDB.get(fixedArtistName) is None:
-                artistNameDB[fixedArtistName] = []
-            artistNameDB[fixedArtistName].append(olderArtistName)
-        print("  Forced reload of {0} artist names".format(len(artistNameDB)))
-        print("  Found {0} names".format(len(dbRenames)))
-        self.checkForRecursives()
-        
-        self.artistNameDB = artistNameDB
-        self.dbRenames    = dbRenames
-        if save is True:
-            print("Will save everything")
-            self.save()
-            self.loadRenames()
-            self.saveRenames()
-            
-        
 
-    
-    
+        
     ##########################################################################################
-    # Finder Functions
+    #
+    # Merge
+    #
     ##########################################################################################
-    def findArtist(self, artistName):
-        matches = findNearest(artistName, list(self.artistNameDB.keys()), 3, cutoff=0.85)
-        for matchName,renameNames in {matchName: self.artistNameDB[matchName] for matchName in matches}.items():
-            print(matchName)
-            for renameName in renameNames:
-                print("\t",renameName)
-
-    
-    ##########################################################################################
-    # I/O
-    ##########################################################################################
-    def createRenamesFromDB(self, mdbmap, dbRenames):
-        artistNameDB = {x[0]: [] for x in mdbmap.getArtists()}
-
-        errs = []
-        for olderArtistName,fixedArtistName in dbRenames.items():
-            if artistNameDB.get(olderArtistName) is not None:
-                errs.append("Recursive definition! Key [{0}] and Value [{1}]".format(artistNameDB.get(olderArtistName),olderArtistName))
-        if len(errs) > 0:
-            print("Found The Following Recursive Errors:")
-            for i,err in enumerate(errs):
-                print(i,'\t',err)
-            raise ValueError("Need To Fix Recursive Errors")
+    def merge(self, newRenames, test=False):
+        print("Merging New Renames With Master Renames Data")
+        newRenames = Series(newRenames) if not isinstance(newRenames, Series) else newRenames
+        testManualRenames = self.manualRenames.append(newRenames)
+        
+        retval,testManualRenames  = self.duplicateIndexTest(testManualRenames)
+        if retval is False:
+            raise ValueError("There are duplicate key,values in the merged data".format())
+        retval = self.recursiveTest(testManualRenames)
+        if retval is False:
+            raise ValueError("There are recursive key,values in the merged data".format())
             
-
-        for olderArtistName,fixedArtistName in dbRenames.items():
-            if artistNameDB.get(fixedArtistName) is None:
-                artistNameDB[fixedArtistName] = []
-            artistNameDB[fixedArtistName].append(olderArtistName)
-                   
-        saveFile(idata=artistNameDB, ifile=self.artistNameDBname)
-        
-        
-    def removeArtist(self, artistName):
-        if self.artistNameDB.get(artistName) is not None:
-            print("Removing {0} from artist keys".format(artistName))
-            del self.artistNameDB[artistName]
-            self.save()
-            self.loadRenames()
-            self.saveRenames()
+        if test is False:
+            self.saveData(fast=True, local=False)
+            self.saveData(fast=False, local=True)
         else:
-            print("Could not not find {0} in artist keys.".format(artistName))
-            
-            
-    def getRenames(self):
-        return self.dbRenames
-    
-    def getArtistNames(self):
-        return self.artistNameDB
-    
-    
-    def addRenames(self, dbRenames):
-        print("Trying to add {0} renamed artist keys".format(len(dbRenames)))
-        errs = []
-        for olderArtistName,fixedArtistName in dbRenames.items():
-            if self.artistNameDB.get(olderArtistName) is not None:
-                errs.append("Recursive definition! Key [{0}] and Value [{1}]".format(self.artistNameDB.get(olderArtistName),olderArtistName))
-
-        if len(errs) > 0:
-            print("Found The Following Recursive Errors:")
-            for i,err in enumerate(errs):
-                print(i,'\t',err)
-            raise ValueError("Need To Fix Recursive Errors")
-            
-        print("There are currently {0} artist keys.".format(len(self.artistNameDB)))
-        print("There are currently {0} renamed artist keys.".format(len(self.dbRenames)))
-        for olderArtistName,fixedArtistName in dbRenames.items():
-            self.dbRenames[olderArtistName] = fixedArtistName
-            if self.artistNameDB.get(fixedArtistName) is None:
-                self.artistNameDB[fixedArtistName] = []
-            self.artistNameDB[fixedArtistName].append(olderArtistName)
-        print("There are currently {0} artist keys.".format(len(self.artistNameDB)))
-        print("There are currently {0} renamed artist keys.".format(len(self.dbRenames)))
-        self.checkForRecursives()
-    
-        
-    def checkForRecursives(self, artistNameDB=None):
-        if artistNameDB is None:
-            artistNameDB = self.artistNameDB
-        errs = []
-        for artistName,namesToBeFixed in artistNameDB.items():
-            for nameToBeFixed in namesToBeFixed:
-                if self.artistNameDB.get(nameToBeFixed) is not None:
-                    errs.append("Recursive found! Key [{0}] and Values [{1},{2}]".format(artistName,nameToBeFixed,self.artistNameDB.get(nameToBeFixed)))
-                    
-        if len(errs) > 0:
-            print("Found The Following Recursive Errors:")
-            for i,err in enumerate(errs):
-                print(i,'\t',err)
-            raise ValueError("Need To Fix Recursive Errors")
+            print("Only Testing The Machinery And Not Saving Results")
